8f735d29ff27155fb5c9ca0b615ff4e251eafae1
trevornei <117675657+trevornei@users.noreply.github.com>
2024-10-20 12:29:25 -0600

    Added an extra console.log() with the localhost string + port

--- temp/simple_server.js
+++ temp/simple_server.js
@@ -14,8 +14,12 @@ const server = http.createServer(function (req, res) {
     // 200 = status_code that we are sending back to the client.
     // The {object} tells the client in the packet head (being sent over the network to the browser) that the server is sending back html.
     res.writeHead(200, {'content-type':'text/html'});
+    // .createReadStream() = node method
+    //    A memory efficient way of reading data from a file. Essentially, the data is chunked in batches instead of being sent all at once.
+    // .pipe() helps handle reading the data from the server and writing the data to the client/desintion.
     fs.createReadStream('index.html').pipe(res);
 });
 // Tell the server about the port.
 server.listen(PORT);
 console.log(`Your server started on port: ${PORT}`);
+console.log(`Open this server instane here: localhost:${PORT}`)
