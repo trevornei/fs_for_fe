70dd15228a528fd9713d6c04a78fa56aea98be64
trevornei <117675657+trevornei@users.noreply.github.com>
2024-10-20 12:14:37 -0600

    Update

--- temp/simple_server.js
+++ temp/simple_server.js
@@ -9,7 +9,10 @@ const fs = require('fs');
 // CONST variables should generally be all caps for readability.
 const PORT = 3000;
 
+// Every server has two components: 1. requests  2.responses
 const server = http.createServer(function (req, res) {
+    // 200 = status_code that we are sending back to the client.
+    // The {object} tells the client in the packet head (being sent over the network to the browser) that the server is sending back html.
     res.writeHead(200, {'content-type':'text/html'});
     fs.createReadStream('index.html').pipe(res);
 });
